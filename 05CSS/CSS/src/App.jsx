e1f1d61e3c106ab640dc115536712ba3ece7dde3
Sandip Chavan <sandipchavan202004@gmail.com>
2026-01-02 22:27:40 +0530

    add new contetnt

---
+++ 05CSS/CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/Button'
+
+const App = () => {
+  return (
+    <div>
+      <div>
+          <Button/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ 06_tailwind/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen bg-gray-900'>
+      <h1 className='text-white text-4xl text-center p-12'>SANDIP CHAVAN</h1>
+    </div>
+  )
+}
+
+export default App
