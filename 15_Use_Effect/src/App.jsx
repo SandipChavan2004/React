897c93f47c09ca5a224b4116510eda0dd741ce42
Sandip Chavan <sandipchavan202004@gmail.com>
2026-01-16 13:35:34 +0530

    use effect

---
+++ 15_Use_Effect/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+
+  const [num, setNum] = useState(0);
+
+  useEffect(function(){
+    console.log("Use effect is Running!");
+  },[]);
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={()=>{
+        setNum(num+1);
+      }}>Click</button>
+    </div>
+  )
+}
+
+export default App
